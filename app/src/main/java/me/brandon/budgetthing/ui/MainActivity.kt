d2307683189577e15132607844ca34c612de11cb
Brandom Rodriguez <brandomrobor@gmail.com>
2023-02-05 22:10:24 -0600

    Add UI implementation of navigation component to main activity

--- app/src/main/java/me/brandon/budgetthing/ui/MainActivity.kt
+++ app/src/main/java/me/brandon/budgetthing/ui/MainActivity.kt
@@ -2,14 +2,31 @@ package me.brandon.budgetthing.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import me.brandon.budgetthing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.appToolbar)
+        val navController =
+            (supportFragmentManager.findFragmentById(binding.appFragmentContainer.id) as NavHostFragment).navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return binding.appFragmentContainer.findNavController()
+            .navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
