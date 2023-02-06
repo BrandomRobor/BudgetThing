35f3bbe3829987d2d892e7a54792fabc063e9398
Brandom Rodriguez <brandomrobor@gmail.com>
2023-02-05 21:13:02 -0600

    Move MainActivity to ui package and add view binding to activity

---
+++ app/src/main/java/me/brandon/budgetthing/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package me.brandon.budgetthing.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.brandon.budgetthing.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
