fee2d75cf834f139d1441b3f7227473a5ea5e7a8
Brandom Rodriguez <brandomrobor@gmail.com>
2023-02-05 20:43:50 -0600

    Create entities' dao with basic CRUD queries

---
+++ app/src/main/java/me/brandon/budgetthing/db/dao/TransactionCategoryDao.kt
@@ -0,0 +1,21 @@
+package me.brandon.budgetthing.db.dao
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import me.brandon.budgetthing.db.entity.TransactionCategory
+import me.brandon.budgetthing.util.CategoryType
+
+@Dao
+interface TransactionCategoryDao {
+    @Insert
+    suspend fun insertCategory(category: TransactionCategory): Long
+
+    @Update
+    suspend fun updateCategory(category: TransactionCategory): Long
+
+    @Delete
+    suspend fun deleteCategory(category: TransactionCategory)
+
+    @Query("SELECT * FROM TransactionCategory WHERE type = :type")
+    fun getAllCategoriesOfType(type: CategoryType): Flow<List<TransactionCategory>>
+}
---
+++ app/src/main/java/me/brandon/budgetthing/db/dao/TransactionDao.kt
@@ -0,0 +1,20 @@
+package me.brandon.budgetthing.db.dao
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import me.brandon.budgetthing.db.entity.Transaction
+
+@Dao
+interface TransactionDao {
+    @Insert
+    suspend fun insertTransaction(transaction: Transaction): Long
+
+    @Update
+    suspend fun updateTransaction(transaction: Transaction): Long
+
+    @Delete
+    suspend fun deleteTransaction(transaction: Transaction)
+
+    @Query("SELECT * FROM `Transaction`")
+    fun getAllTransactions(): Flow<List<Transaction>>
+}
