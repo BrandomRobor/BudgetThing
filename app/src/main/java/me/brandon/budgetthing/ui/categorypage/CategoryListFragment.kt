2b3fc809edb69f971b75edd8fced1047890c95e9
Brandom Rodriguez <brandomrobor@gmail.com>
2023-02-15 09:26:56 -0600

    Move recycler view to its own fragment In preparation to add view pager

---
+++ app/src/main/java/me/brandon/budgetthing/ui/categorypage/CategoryListFragment.kt
@@ -0,0 +1,52 @@
+package me.brandon.budgetthing.ui.categorypage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import me.brandon.budgetthing.databinding.FragmentCategoryListBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class CategoryListFragment : Fragment() {
+    private var _binding: FragmentCategoryListBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: CategoryPageViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCategoryListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = CategoryListAdapter()
+
+        binding.apply {
+            categoryListRecyclerView.adapter = adapter
+            categoryListRecyclerView.setHasFixedSize(true)
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.categoriesListState.collectLatest {
+                    adapter.submitList(it)
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/me/brandon/budgetthing/ui/categorypage/CategoryPageFragment.kt
+++ app/src/main/java/me/brandon/budgetthing/ui/categorypage/CategoryPageFragment.kt
@@ -5,19 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.findNavController
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import me.brandon.budgetthing.databinding.FragmentCategoryPageBinding
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CategoryPageFragment : Fragment() {
     private var _binding: FragmentCategoryPageBinding? = null
     private val binding get() = _binding!!
-    private val viewModel: CategoryPageViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,18 +24,6 @@ class CategoryPageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-            val adapter = CategoryListAdapter()
-            categoryPageRecyclerView.setHasFixedSize(true)
-            categoryPageRecyclerView.adapter = adapter
-
-            viewLifecycleOwner.lifecycleScope.launch {
-                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                    viewModel.categoriesListState.collectLatest {
-                        adapter.submitList(it)
-                    }
-                }
-            }
-
             categoryPageAddFab.setOnClickListener {
                 val action =
                     CategoryPageFragmentDirections.actionNavDrawerCategoryEntryToCategoryFormFragment()
