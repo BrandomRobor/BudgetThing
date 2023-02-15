4149041f01ba24c15cf6e356ed046bdbc63a86cd
Brandom Rodriguez <brandomrobor@gmail.com>
2023-02-14 23:57:18 -0600

    Add submit function to category form

--- app/src/main/java/me/brandon/budgetthing/ui/categoryform/CategoryFormFragment.kt
+++ app/src/main/java/me/brandon/budgetthing/ui/categoryform/CategoryFormFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import me.brandon.budgetthing.databinding.FragmentCategoryFormBinding
+import me.brandon.budgetthing.util.CategoryType
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CategoryFormFragment : Fragment() {
     private var _binding: FragmentCategoryFormBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: CategoryFormViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +29,34 @@ class CategoryFormFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             categoryFormNameField.requestFocus()
+
+            catgoryFormDoneFab.setOnClickListener {
+                onFormSubmit()
+            }
+            categoryFormDescriptionField.editText!!.setOnEditorActionListener { _, action, _ ->
+                if (action == EditorInfo.IME_ACTION_DONE) {
+                    onFormSubmit()
+                    return@setOnEditorActionListener true
+                }
+                return@setOnEditorActionListener false
+            }
+        }
+    }
+
+    private fun onFormSubmit() {
+        binding.apply {
+            val selectedType = when (categoryFormButtonGroup.checkedButtonId) {
+                categoryFormIncomeButton.id -> CategoryType.INCOME
+                categoryFormExpenseButton.id -> CategoryType.EXPENSE
+                else -> CategoryType.INCOME
+            }
+
+            viewModel.submitCategory(
+                categoryFormNameField.editText!!.text.toString(),
+                selectedType,
+                categoryFormDescriptionField.editText!!.text.toString()
+            )
+            findNavController().navigateUp()
         }
     }
 
